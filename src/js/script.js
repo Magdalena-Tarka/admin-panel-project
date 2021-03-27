2a32ee1876657c79d86ddfc733315ff5f78c5a5f
Magdalena Tarka <mxtarka@gmail.com>
2021-03-27 21:20:08 +0100

    Fix bugs from previous commit vol.3

--- src/js/script.js
+++ src/js/script.js
@@ -8,19 +8,13 @@
   const ctx = document.getElementById('myChart').getContext('2d');
 
   const chart = new Chart(ctx, {
-    // 1
     type: 'bar',
     data: {
-    // 2
       labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
-      // 3
       datasets: [{
-        // 4
         label: 'Signups',
-        // 5
         backgroundColor: '#8DBEC8',
         borderColor: '#8DBEC8',
-        // 6
         data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
       },
       {
@@ -34,7 +28,6 @@
         backgroundColor: '#71B374',
         borderColor: '#71B374',
         data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
-        // 7
         hidden: true,
       }]
     },
@@ -57,12 +50,11 @@
 
   /* Modals */
 
-  //Zamykanie modala poprzez usunięcie klasy 'show' z owerlaya
+  //CLOSE MODAL
   function closeModal() {
     document.getElementById('overlay').classList.remove('show');
   }
 
-  //Podpinamy funkcje pod przyciski 'js--close-modal'
   document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
     btn.addEventListener('click', function(e) {
       e.preventDefault();
@@ -70,21 +62,19 @@
     });
   });
 
-  //Dodajemy możliwość zamknięcia modala po kliknięciu w tło overlaya
   document.querySelector('#overlay').addEventListener('click', function(e) {
     if(e.target === this) {
       closeModal();
     }
   });
 
-  //oraz zamykanie przez kliknięcie ESC na klawiaturze
   document.addEventListener('keyup', function(e) {
     if(e.keyCode === 27) {
       closeModal();
     }
   });
 
-  // OTWIERANIE MODALA
+  //OPEN MODAL
   function openModal(modal) {
     document.querySelectorAll('#overlay > *').forEach(function(modal) {
       modal.classList.remove('show');
@@ -93,69 +83,57 @@
     document.querySelector(modal).classList.add('show');
   }
 
-  // Nasłuchiwacz na wszystkie modale (do elementu nalezy tylko dodać class="modal-btn" oraz atrybut data-modal="id modala")
+  //Event listenners for all modals (just add class="modal-btn" to the DOM element)
   for(let modalElem of document.querySelectorAll('.modal-btn')) {
 
     modalElem.addEventListener('click', function(e) {
       e.preventDefault();
-      let clickedModal = e.target.getAttribute('data-modal') || e.currentTarget.getAttribute('data-modal');
+      let clickedModal = this.getAttribute('data-modal');
       openModal('#' + clickedModal);
     });
   }
 
+
   /* Init Pages */
-
-  const mainNav = {
     
-    initPages: function() {
+  function initPages() {
 
-      this.pages = document.querySelector('#sidebar-menu').children;
-      this.navLinks = document.querySelectorAll('.main-nav-item');
-      console.log('this.pages:', this.pages);
-      console.log('this.navLinks:', this.navLinks);
+    this.pages = document.querySelectorAll('.subpage-wrapper');
+    this.navLinks = document.querySelectorAll('.main-nav-item');
   
-      const idFromHash = window.location.hash.replace('#/', '');
-      let pageMatchingHash = this.pages[0].id;
+    const idFromHash = window.location.hash.replace('#/', '');
+    let pageMatchingHash = this.pages[0].id;
   
-      for(let page of this.pages) {
-        if(page.id == idFromHash) {
-          pageMatchingHash = page.id;
-          break;
-        }
+    for(let page of this.pages) {
+      if(page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
       }
-      console.log('pageMatchingHash:', pageMatchingHash);
-      this.activatePage(pageMatchingHash);
+    }
+    this.activatePage(pageMatchingHash);
   
-      for(let link of this.navLinks) {
-        link.addEventListener('click', function(e) {
-          const clickedElement = this;
-          console.log('1. clickedElement:', clickedElement);
-          e.preventDefault();
+    for(let link of this.navLinks) {
+      link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const id = this.querySelector('a').getAttribute('href').replace('#', '');
+        activatePage(id);
+        window.location.hash = '#/' + id;
+      });
+    }
+  }
   
-          const id = clickedElement.getAttribute('data-id').replace('#', '');
-          mainNav.activatePage(id);
-          window.location.hash = '#/' + id;
-        });
-      }
-    },
-  
-    activatePage: function(pageId) {
+  function activatePage(pageId) {
       
-      for(let page of this.pages) {
-        page.classList.toggle('activePage', page.id == pageId);
-      }
+    for(let page of this.pages) {
+      page.classList.toggle('active-page', page.id == pageId);
+    }
 
-      for(let link of this.navLinks) {
-        link.classList.toggle('activePage', link.getAttribute('data-id') == '#' + pageId
-        );
-      }
-    },
+    for(let link of this.navLinks) {
+      link.classList.toggle('active-page', link.getAttribute('data-id') == '#' + pageId
+      );
+    }
+  }
       
-    init: function() {
-      console.log('*** Welcome ***');
-      this.initPages();
-    },
-  };
-  mainNav.init();
-
+  initPages();
+  console.log('*** Welcome ***');
 }
